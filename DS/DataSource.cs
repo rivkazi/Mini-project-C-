2464a016e95ea0df08ee9f1959e1baf6abde5ff2
rivkazi <67023971+rivkazi@users.noreply.github.com>
2020-06-22 01:28:52 +0300

    DS

---
+++ DS/DataSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+
+namespace DS
+{
+    public class DataSource
+    {
+        public static List<GuestRequest> listGuestRequests = new List<GuestRequest>();
+        public static List<HostingUnit> listHostingUnits = new List<HostingUnit>();
+        public static List<Order> listOrders = new List<Order>();
+    }
+}
